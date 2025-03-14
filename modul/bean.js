6b579b20e6068dd35bf0771a11f3a4da45a5c84a
Nihat <85920289+zacnider@users.noreply.github.com>
2025-03-14 22:24:25 +0300

    Update bean.js

--- modul/bean.js
+++ modul/bean.js
@@ -52,8 +52,8 @@ const CHAIN_ID = 10143;
 const BEAN_SWAP_ROUTER_ADDRESS = ROUTER_CONTRACT; 
 const WETH_ADDRESS = WMON_CONTRACT; 
 
+// WMON'u TOKEN_ADDRESSES'den çıkarın
 const TOKEN_ADDRESSES = {
-    "WMON": WMON_CONTRACT, 
     "USDC": USDC_CONTRACT, 
     "BEAN": BEAN_CONTRACT,  
     "JAI ": JAI_CONTRACT
@@ -63,6 +63,7 @@ const erc20Abi = [
     { "constant": true, "inputs": [{ "name": "_owner", "type": "address" }], "name": "balanceOf", "outputs": [{ "name": "balance", "type": "uint256" }], "type": "function" },
     { "constant": false, "inputs": [{ "name": "_spender", "type": "address" }, { "name": "_value", "type": "uint256" }], "name": "approve", "outputs": [{ "name": "", "type": "bool" }], "type": "function" }
 ];
+
 
 async function connectToRpc() {
     for (const url of RPC_URLS) {
